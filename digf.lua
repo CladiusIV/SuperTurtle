072b70d73606ea1bcd2cb7de4d562865ed5337b5
Claudio <claudioslopez04@gmail.com>
2026-02-17 22:25:44 -0300

    Add dig turtle 2 block height Add height variable

--- digf.lua
+++ digf.lua
@@ -1,5 +1,10 @@
--- just a simple digging turtle.
--- single command turtle.
+--
+-- Just a simple digging turtle.
+-- Single command turtle, fast 2 block mining.
+--
+
+-- how many blocks to mine forward.
+local mineLength = 20
 
 local function moveForward()
     -- Check if there's a block in front
@@ -13,9 +18,10 @@ local function moveForward()
 
         print("Dig result: " .. tostring(digSuccess), digData and digData.name or "no data")
         if not digSuccess then
-            print("Failed to dig - may be protected or undiggable")
+            print("Failed to dig - may be protected or undiggable" .. (digData and digData.name or " unknown block"))
             return false
         end
+        turtle.digUp()
         print("Mined block successfully")
     else
         print("No block in front, moving forward")
@@ -30,11 +36,11 @@ local function moveForward()
     return true
 end
 
--- Loop 15 times to move forward and mine blocks
-for i = 1, 15 do
+-- Loop mineLength times to move forward and mine blocks
+for i = 1, mineLength do
     local successInfo, dataInfo = turtle.inspect()
     if moveForward() then
-        print("Moved block " .. i .. " of 15. " .. (dataInfo.name or "unknown block"))
+        print("Moved block " .. i .. " of " .. mineLength .. ". " .. (dataInfo.name or "unknown block"))
     else
         print("Failed to move - stopping: " .. (dataInfo.name or "unknown reason"))
         break
